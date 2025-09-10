cf05d092e4937290e76b02936e5b8a99513f1878
Massimo Romanelli <massimoromanelli@mac.home>
2025-09-10 14:23:26 -0400

    finished scrapping tutorial

--- crawler.py
+++ crawler.py
@@ -2,6 +2,9 @@ import requests
 from bs4 import BeautifulSoup
 
 URL = "https://realpython.github.io/fake-jobs/"
+
+
+#https://realpython.github.io/fake-jobs/
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
@@ -15,11 +18,55 @@ job_cards = results.find_all("div", class_="card-content")
 
 
 #Adding .text after each element to get only the text
-for job_card in job_cards:
+#.strip() removes any extra whitespace
+
+
+# for job_card in job_cards:
+#     title_element = job_card.find("h2", class_="title")
+#     company_element = job_card.find("h3", class_="company")
+#     location_element = job_card.find("p", class_="location")
+#     print(title_element.text.strip())
+#     print(company_element.text.strip())
+#     print(location_element.text.strip())
+#     print()
+
+
+python_jobs = results.find_all(
+    "h2", string=lambda text: "python" in text.lower()
+    )
+#to get information of each job we must navigate to the great-grandparent element
+
+python_jobs_info = [
+    h2_element.parent.parent.parent for h2_element in python_jobs
+]
+
+# for job_card in python_jobs_info:
+#     title_element = job_card.find("h2", class_="title")
+#     company_element = job_card.find("h3", class_="company")
+#     location_element = job_card.find("p", class_="location")
+#     print(title_element.text.strip())
+#     print(company_element.text.strip())
+#     print(location_element.text.strip())
+#     print()
+
+#getting urls of buttons 
+
+# for job_card in python_jobs_info:
+#     links = job_card.find_all("a")
+#     for link in links:
+#         link_url = link["href"]
+#         print(f"Apply here: {link_url}\n")
+
+#we only want apply button url so we can do this by indexing
+
+for job_card in python_jobs_info:
     title_element = job_card.find("h2", class_="title")
     company_element = job_card.find("h3", class_="company")
     location_element = job_card.find("p", class_="location")
-    print(title_element.text)
-    print(company_element.text)
-    print(location_element.text)
-    print()
+    print(title_element.text.strip())
+    print(company_element.text.strip())
+    print(location_element.text.strip())
+
+    link = job_card.find_all("a")[1]["href"]
+    print(f"Apply here: {link}\n")
+
